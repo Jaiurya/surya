8931d397a79f5ac99050be4f77742621042ab177
Surya <thrymr@thrymr.com>
2023-01-03 16:37:29 +0530

    renamed:    Multiplication.java -> gitignore/Division.java

--- gitignore/Division.java
+++ gitignore/Division.java
@@ -1,14 +1,14 @@
-public class Multiplication{
+public class Division{
          public static void main(String[] args)
      {
        int num1=26;
        int num2=24;
-       int mult = 0;
+       int div = 0;
            
        System.out.println("num1 =  " + num1);
        System.out.println("num2 =  " + num2);
 
-       mult = num1 * num2;
-       System.out.println(" Multiplication =  " + mult);
+       div = num1 / num2;
+       System.out.println(" Division =  " + div);
      }
 }
