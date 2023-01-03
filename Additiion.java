4d4326af7935e6cb2918b717cb6cd36232c6a4e0
Surya <thrymr@thrymr.com>
2023-01-03 11:52:25 +0530

    First Commit

---
+++ Additiion.java
@@ -0,0 +1,14 @@
+public class Additiion{
+         public static void main(String[] args)
+     {
+       int num1=26;
+       int num2=24;
+      
+           
+       System.out.println("num1 =  " + num1);
+       System.out.println("num2 =  " + num2);
+
+       sum = num1 + num2;
+       System.out.println(" The sum =  " + num1 + num2);
+     }
+}
---
+++ AssignmentOperators.java
@@ -0,0 +1,26 @@
+public class  AssignmentOperators{
+         public static void main(String[] args)
+     {
+       int num1=26;
+       int num2=24;
+    
+           
+              System.out.println("num1 =  " +    (num1 = 26) );  
+             System.out.println("num1 =  " +    (num1 += 4) );
+             System.out.println("num1 =  " +    (num1 -= 4)  );
+             System.out.println("num1 =  " +    (num1 *= 4)  );
+             System.out.println("num1 =  " +    (num1 /= 4) );
+             System.out.println("num1 =  " +    (num1 %= 4) );
+
+
+
+             System.out.println("num2 =  " +    (num2 = 24));  
+             System.out.println("num2 =  " +    (num2 += 6));
+             System.out.println("num2 =  " +    (num2 -= 6));
+             System.out.println("num2 =  " +    (num2 *= 6));
+             System.out.println("num2 =  " +    (num2 /= 6));
+             System.out.println("num2 =  " +    (num2 %= 6));
+
+   
+     }
+}
