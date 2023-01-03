51d0e64c78e8e65862abe50fb0c92f40d4567bfe
Surya <thrymr@thrymr.com>
2023-01-03 15:08:53 +0530

    Division.javanew file:   Division.java

---
+++ Division.java
@@ -0,0 +1,14 @@
+public class Division{
+         public static void main(String[] args)
+     {
+       int num1=26;
+       int num2=24;
+       int div = 0;
+           
+       System.out.println("num1 =  " + num1);
+       System.out.println("num2 =  " + num2);
+
+       div = num1 / num2;
+       System.out.println(" Division =  " + div);
+     }
+}
