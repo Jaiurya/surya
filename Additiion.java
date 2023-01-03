7c1878a901395c8323ceb07202dee090126fbf25
Surya <thrymr@thrymr.com>
2023-01-03 16:21:02 +0530

    modified:   Additiion.java

--- Additiion.java
+++ Additiion.java
@@ -5,9 +5,9 @@ public class Additiion{
        int num2=24;
       
            
-       System.out.println("num1 =  " + num1);
+  /**     System.out.println("num1 =  " + num1);
        System.out.println("num2 =  " + num2);
-
+*/
        sum = num1 + num2;
        System.out.println(" The sum =  " + num1 + num2);
      }
