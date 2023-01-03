72051bd415065fff734e4eb81a2b73baf555f83b
Surya <thrymr@thrymr.com>
2023-01-03 14:29:55 +0530

    First Commit

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+public class Multiplication{
+         public static void main(String[] args)
+     {
+       int num1=26;
+       int num2=24;
+       int mult = 0;
+           
+       System.out.println("num1 =  " + num1);
+       System.out.println("num2 =  " + num2);
+
+       mult = num1 * num2;
+       System.out.println(" Multiplication =  " + mult);
+     }
+}
